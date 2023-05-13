0eb2e33aa56fee700dafff98bd7ef6fb2d971363
kimkyuuuuri <rnflrnfl0633@naver.com>
2023-05-13 20:09:22 +0900

    drama crawling

--- celeb/movie_actor_scroll.py
+++ celeb/movie_actor_scroll.py
@@ -20,16 +20,15 @@ month=1
 index=3
 num=0
 
-while period < 3 and year <3: 
+formdata_dict = dict()
+formdata_dict['section'] = 'actor'
+formdata_dict['period_start'] = '2012-4'
+formdata_dict['gender'] = 'all'
+
+while True : 
     
     try:
-        if period ==1 and year==1 and month==4:
-            year=2
-            month=1
-        if period ==1 and year==5:
-            period=2
-            year=1
-
+        
         celeb_category_id=10
         context = ssl._create_unverified_context()
         driver = wd.Chrome('/Users/kimkyuri/Documents/학교공부/4-1/sluv/crawling/celeb/chromedriver_mac_arm64/chromedriver')
@@ -62,11 +61,84 @@ while period < 3 and year <3:
             data_list = eval(actors_str)
             for i in range(len(data_list)):
                 num+=1
-                print(data_list[i])
+                actor_name = data_list[i]
+                #print(data_list[i])
+                actor_url = 'http://www.cine21.com/search/person/?q='
+                actor_url = actor_url + urllib.parse.quote(actor_name)
+                
+
+                req2 = Request(actor_url, headers={'User-Agent': 'Mozilla/5.0'})
+                url = urlopen(req2,context=context).read()
+                _main_page2 = BeautifulSoup(url, 'html.parser')
+                detail=_main_page2.findAll("p")
+                
+
+                
+                for index, data in enumerate(detail):
+                    data2=data.findAll("a")
+                    actor_num=data2[0]['href'].split('=')[1]
+                    print(actor_num)
+                    break
+
+                actor_detail_url= 'http://www.cine21.com/db/person/info/?person_id='
+                actor_detail_url = actor_detail_url + urllib.parse.quote(str(actor_num))
+                
+
+                req2 = Request(actor_detail_url, headers={'User-Agent': 'Mozilla/5.0'})
+                url = urlopen(req2,context=context).read()
+                print(actor_detail_url)
+
+        
+                soup = BeautifulSoup(url,'html.parser')
+                actor_item_dict = dict()
+
+                for li_tag in soup.select('ul.default_info li'):
+                    
+                    actor_item_field = li_tag.select_one('span.tit').text 
+                 
+                    actor_item_value = re.sub('<span.*?>.*?</span>','',str(li_tag))
+                    actor_item_value = re.sub('<.*?>','',actor_item_value) 
+                  
+                    regex = re.compile(r'[\n\r\t]')
+                    actor_item_value = regex.sub('',actor_item_value)
+                    
+                    actor_item_dict[actor_item_field] = actor_item_value
+
+                if actor_item_dict['성별'] == '남':
+                    celeb_category_id= 11
+                else:
+                    celeb_category_id= 12
+                    
+                    
+
+                naver_url = 'https://dict.naver.com/name-to-roman/translation/?query='
+                name_url = naver_url + urllib.parse.quote(actor_name)
+
+                req = Request(name_url, headers={'User-Agent': 'Mozilla/5.0'})
+                res = urlopen(req,context=context)
+
+                html = res.read().decode('utf-8')
+                bs = BeautifulSoup(html, 'html.parser')
+                name_tags = bs.select('#container > div > table > tbody > tr > td > a')
+                eng_names = [name_tag.text for name_tag in name_tags]
+                
+               
+             
+                print(actor_name)
+                #print(num)
+                if num==500:
+                    wb.save("actor_drama.xlsx")
+                    break
+                    
+                sheet.append([num,celeb_category_id, eng_names[0],actor_name])
+                    
 
 
-        if num==500:
-            break
+
+        #if num==5:
+                                   
+         #   wb.save("drama_actor.xlsx")
+          #  break
         
        
     except Exception as e:
@@ -74,6 +146,5 @@ while period < 3 and year <3:
         print(e)
         continue
                    
-                       
-wb.save("drama_actor.xlsx")
+
 
