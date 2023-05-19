75d9324a668df711918c2bc6b171d6661bf3ef10
kimkyuuuuri <rnflrnfl0633@naver.com>
2023-05-19 23:43:40 +0900

    정리

---
+++ brand/musinsa(공백제거)0515.py
@@ -0,0 +1,54 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+import json
+import ssl
+import requests
+import openpyxl
+import urllib.request
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+
+
+def allbrand(page):
+    context = ssl._create_unverified_context()
+    ssl._create_default_https_context = ssl._create_unverified_context
+    req = Request('https://www.musinsa.com/dp/fragments/brands?categoryCode=&type=&sortCode=BRAND_RANK&listViewType=small&page='+str(page), headers={'User-Agent': 'Mozilla/5.0'})
+    url = urlopen(req,context=context).read()
+    _main_page = BeautifulSoup(url, 'html.parser')    
+    upper_category =  _main_page.findAll('a', 'gtm-catch-click')
+    eng_name=""
+    kr_name=""
+    img_link=""
+    for i,brand in enumerate(upper_category):
+        if(i%2==0):
+            link='https://www.musinsa.com'+brand['href']
+            req2 = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            url = urlopen(req2,context=context).read()
+            _main_page2 = BeautifulSoup(url, 'html.parser')
+            category=_main_page2.findAll("div","brand_logo brandLogo")
+            for i2,brand_for_img in enumerate(category):
+               
+                select=brand_for_img.select_one('img')
+                kr_name=select['alt'].split('(')[0]
+                eng_name=select['alt'].split('(')[1].split(')')[0]
+                img_link="https:"+select['src']
+                image_name = eng_name.replace(" ", "")
+                image_link = "https://sluv-brands.s3.ap-northeast-2.amazonaws.com/brand_img/"+image_name+".png"
+                sheet.append([eng_name,kr_name,image_link])
+               # print(image_name)
+                try:
+                    urllib.request.urlretrieve(img_link,image_name)
+                    #sheet.append([eng_name,kr_name,image_link])
+                except:
+                    continue
+                    
+     
+
+for i in range(1,356):
+
+    allbrand(i)
+    print(i)
+#wb.save("musinsa_brand_공백제거.xlsx")
+
---
+++ brand/sivillage_공백제거_0515.py
@@ -0,0 +1,69 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+import json
+import ssl
+import requests
+import openpyxl
+import urllib.request
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+
+
+def allbrand():
+    context = ssl._create_unverified_context()
+    ssl._create_default_https_context = ssl._create_unverified_context
+    #req = Request('https://hbx.com/kr/men/brands?utm_source=hypebeast.kr', headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request('https://www.sivillage.com/dispctg/brandAtoZTab.siv', headers={'User-Agent': 'Mozilla/5.0'})
+    url = urlopen(req,context=context).read()
+    _main_page = BeautifulSoup(url, 'html.parser')    
+    upper_category =  _main_page.findAll('p','brand-index__item--description')
+    upper_category2=_main_page.findAll('p','brand-index__item--brand gold-mark')
+    eng_name=""
+    kr_name=""
+    img_link=""
+ 
+    link=_main_page.findAll('a','brand-index__item--text')
+    
+    for i,brand in enumerate(link):
+            print(i)
+            select=brand['href'].split("'")[-2]
+            a = brand.find('p','brand-index__item--description')
+            kor_name = a.text.split("\n")[-1].split(' ')[0]
+            b = brand.find('p','brand-index__item--brand')
+            eng_name = b.text.split("\n")[-1].split(' ')[0]
+            
+            link='https://www.sivillage.com/dispctg/initBrandGoodsCtg.siv?disp_ctg_no='+select
+            
+            req2 = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            url = urlopen(req2,context=context).read()
+            _main_page2 = BeautifulSoup(url, 'html.parser')
+            category=_main_page2.findAll("div","brand_logo")
+            
+            for i2,brand_for_img in enumerate(category):
+                select=brand_for_img.select_one('img')
+                img_link=select['src']
+                image_name = eng_name.replace(" ", "")
+                image_link = "https://sluv-brands.s3.ap-northeast-2.amazonaws.com/brand_img/"+image_name+".png"
+                #sheet.append([eng_name,kor_name,image_link])
+               
+                try:
+                    
+                    
+                    urllib.request.urlretrieve(img_link,image_name)
+                    sheet.append([eng_name,kor_name,image_link])
+                  
+                    
+                except:
+                    continue
+            
+    
+        
+                    
+    
+allbrand()
+wb.save("m_sivillage_공백제거.xlsx")
+
+ 
+
